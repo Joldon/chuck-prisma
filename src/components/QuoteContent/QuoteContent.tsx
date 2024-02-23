146bf60979ec3d3ff628c08f87f5189ad992d7db
joldon <79482239+Joldon@users.noreply.github.com>
2024-02-23 14:27:50 +0100

    working fetch component with refresh button

--- src/components/QuoteContent/QuoteContent.tsx
+++ src/components/QuoteContent/QuoteContent.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+// Create the interface for the quote type
+interface Quote {
+  id: string;
+  value: string;
+}
+
+// Define the getQuotes function
+const getQuotes = async (): Promise<Quote> => {
+  const res = await fetch(
+    "https://api.chucknorris.io/jokes/random?category=dev"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+};
 
 const QuoteContent = () => {
-  return <div>QuoteContent</div>;
+  // Use state to store the fetched quotes
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  // Use effect to fetch quotes when the component mounts
+
+  const fetchQuotes = async () => {
+    try {
+      const fetchedQuote = await getQuotes();
+      setQuote(fetchedQuote);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const handleRefreshQuote = () => {
+    fetchQuotes();
+  };
+
+  // Check if quote is not null before trying to access its properties
+  return (
+    <div>
+      QuoteContent
+      {quote && (
+        <div>
+          <Image
+            src="/chuck-norris.png" // Use the icon_url from the fetched quote
+            alt="Chuck Norris"
+            width={250}
+            height={250}
+          />
+          <div key={quote.id}>{quote.value}</div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleRefreshQuote}
+          >
+            Refresh Quote
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default QuoteContent;
