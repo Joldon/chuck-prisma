01dc9d5b7fc0416a5a992a8cbb6b59d5d5904c4c
joldon <79482239+Joldon@users.noreply.github.com>
2024-02-27 13:09:19 +0100

    changed datasource provider from sqlite to postgres

---
+++ prisma/migrations/20240227113312_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "TotalVisits" (
+    "id" INTEGER NOT NULL DEFAULT 1,
+    "count" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "TotalVisits_pkey" PRIMARY KEY ("id")
+);
