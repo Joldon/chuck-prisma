0daa54dd62a881af28bcda1ae6009376618da4d0
joldon <joldon@gmail.com>
2024-10-25 14:12:48 +0200

    changed from postgres to sqlite

--- prisma/migrations/20240227113312_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "TotalVisits" (
-    "id" INTEGER NOT NULL DEFAULT 1,
-    "count" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "TotalVisits_pkey" PRIMARY KEY ("id")
-);
